4a5462be387b0e79d75d4d9210789d7eb472ad3e
annechege <annechege780@gmail.com>
2021-05-14 20:15:01 +0300

    adding list of quote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+import { word} from './word';
+....
 export class AppComponent {
-  title = 'word';
+  word:word[] = [
+    {id:1, name:'persistance wears out resistance'},
+    {id:2,name:'Love the life you live'},
+    {id:3,name:'Aspire before you expire'},
+    {id:4,name:'Take time to laugh its the music to the soul'},
+    {id:5,name:'Be a cheerful giver'},
+    {id:6,name:'Never say NO before you give it a try'},
+    {id:6,name:'Get busy living or get busy dying'},
+  ];
 }
--- src/app/word.ts
+++ src/app/word.ts
@@ -1,2 +1,4 @@
-export class Word {
+export class word{
+  id: number;
+  name: string;
 }
