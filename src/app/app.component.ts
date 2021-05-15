421b03f00318cd9a424db882f23b6db4025a2269
annechege <annechege780@gmail.com>
2021-05-15 14:25:08 +0300

    description in class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { word} from './word';
-....
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
 export class AppComponent {
-  word:word[] = [
-    {id:1, name:'persistance wears out resistance'},
-    {id:2,name:'Love the life you live'},
-    {id:3,name:'Aspire before you expire'},
-    {id:4,name:'Take time to laugh its the music to the soul'},
-    {id:5,name:'Be a cheerful giver'},
-    {id:6,name:'Never say NO before you give it a try'},
-    {id:6,name:'Get busy living or get busy dying'},
-  ];
+  
 }
--- src/app/word.ts
+++ src/app/word.ts
@@ -1,4 +1,5 @@
 export class word{
   id: number;
   name: string;
+  description:string;
 }
